83fd94bc8f9eaeb02d5e8c6c525694aecc828dc9
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-05-27 07:28:44 +0100

    add basic chat

---
+++ Creash course/practice/chatapp/index.js
@@ -0,0 +1,23 @@
+const express  = require('express')
+const path = require('path')
+
+const app = express()
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.get('/', (req, res)=> {
+    res.render( 'index', {title : "home page"})
+})
+
+io.on('connection', (socket) =>{
+    socket.on('chat message', (msg)=>{
+        io.emit("chat message", msg)
+    })
+
+})
+PORT  = process.env.PORT  || 8000
+
+http.listen( PORT, ()=> console.log(`Server started at ${PORT}`))
