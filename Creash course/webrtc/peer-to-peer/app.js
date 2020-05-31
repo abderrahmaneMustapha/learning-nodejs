fb9b09527ca97a6ae3601d520b3dd30050cc3a5f
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-05-31 19:09:07 +0100

    add signales to client side

--- Creash course/webrtc/peer-to-peer/app.js
+++ Creash course/webrtc/peer-to-peer/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-let http  = require('http').Server(app)
+const http  = require('http').Server(app)
+const io = require('socket.io')(http)
 
 const port = process.env.PORT || 8000
 
@@ -8,4 +9,40 @@ app.use(express.static('public'))
 
 http.listen(port, ()=>{
     console.log('listening on port', port)
+})
+
+io.on('connection', socket =>{
+    console.log('a user connected')
+    socket.on('create or join', room =>{
+        console.log('create or join a room', room)
+        const myRoom = io.sockets.adapter.rooms[room] || {length : 0}
+        const numClients = myRoom.length
+        console.log(room, 'has', numClients, 'clients')
+
+        if(numClients == 0){
+            socket.join(room)
+            socket.emit('created', room)
+        }else if (numClients == 1){
+            socket.join(room)
+            socket.emit('joined', room)
+        } else{
+            socket.emit('full', room)
+        }
+    })
+
+    socket.on('ready', room =>{
+        socket.broadcast.to(room).emit('ready')
+    })
+
+    socket.on('condidate', room =>{
+        socket.broadcast.to(room).emit('condidate')
+    })
+
+    socket.on('offer', room =>{
+        socket.broadcast.to(room).emit('offer')
+    })
+
+    socket.on('answer', room =>{
+        socket.broadcast.to(room).emit('answer')
+    })
 })
--- Creash course/webrtc/peer-to-peer/public/js/main.js
+++ Creash course/webrtc/peer-to-peer/public/js/main.js
@@ -1,12 +1,17 @@
-let devSelectRoom = document.getElementById('selectRoom')
+let divSelectRoom = document.getElementById('selectRooms')
 let divConsulingRoom = document.getElementById('consultingRoom')
 let inputRoomNumber = document.getElementById('roomNumber')
 let btnGoRoom = document.getElementById('goRoom')
 let localVideo = document.getElementById('localVideo')
 let remoteVideo = document.getElementById('remoteVideo')
 
-
-let roomNumbe, localStream, remoteStream, rtcPeerConnection, isCaller
+console.log(divSelectRoom)
+console.log(divConsulingRoom)
+console.log(inputRoomNumber)
+console.log(btnGoRoom)
+console.log(localVideo)
+console.log(remoteVideo)
+let roomNumber, localStream, remoteStream, rtcPeerConnection, isCaller
 
 const icsServer = {
     'iceServer' : [
@@ -19,3 +24,27 @@ const streamConstraints = {
     audio : true,
     video : true
 }
+
+const socket = io()
+
+btnGoRoom.onclick = () =>{
+    if (inputRoomNumber.value === ''){
+        alert('please type a room number')
+    }else{
+        roomNumber = inputRoomNumber.value
+        socket.emit('create or join', roomNumber)
+        divSelectRoom.style = "display : none"
+        divConsulingRoom.style = "display : block"
+    }
+}
+
+socket.on('created', room=>{
+    navigator.mediaDevices.getUserMedia(streamConstraints)
+    .then( stream=>{
+        localStream = stream
+        localVideo.srcObject = stream
+        isCaller = true
+    }).catch( err =>{
+        console.log(" An error  occurred")
+    })
+})
