0c9b470138d421f99f1f6c79c628f8ef261e7e1b
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-06-21 05:30:03 +0100

    add google code labs road

--- Creash course/practice/expressBasics/index.js
+++ Creash course/practice/expressBasics/index.js
@@ -2,16 +2,10 @@ const express  = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 
-
-
-
-
 const logger = require('./middleware/logger')
 const members = require('./Members');
 
 const app = express()
-
-
 
 // Handle bars middlware
 app.engine('handlebars', exphbs({drfaultLayout: 'main'}));
